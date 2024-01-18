fb9214e7e70a35d1a23646ccc75e825e5fdbfa59
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-18 16:12:51 +0100

    feat: grid con boxEl da js

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,18 @@
 //**  FUNCTIONS
 
-/** */
+function generateGrid(container) {
+  container.innerHTML = "";
+  for (i = 0; i < 100; i++) {
+    const boxEl = generateBox();
+    container.append(boxEl);
+  }
+}
+
+function generateBox() {
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.addEventListener("click", function () {
+    box.classList.toggle("clicked");
+  });
+  return box;
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("js OK");
+const difficultLvl = document.getElementById("difficult");
+const startBtn = document.getElementById("start");
+const gameboard = document.getElementById("gameboard");
+
+startBtn.addEventListener("click", function () {
+  generateGrid(gameboard);
+});
