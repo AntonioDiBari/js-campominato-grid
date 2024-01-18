a4ed6e14b0aef705903a15f422b0c3eb212c925b
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-18 17:39:15 +0100

    feat: modifica numero dei boxEl in base alla difficoltà

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,26 @@
 //**  FUNCTIONS
 
-function generateGrid(container) {
+function generateGrid(container, level) {
   container.innerHTML = "";
-  for (i = 1; i <= 100; i++) {
-    const boxEl = generateBox(i);
+  for (i = 1; i <= level * level; i++) {
+    const boxEl = generateBox(i, level);
     container.append(boxEl);
   }
 }
 
-function generateBox(i) {
+function generateBox(i, level) {
   const box = document.createElement("div");
   box.innerText = i;
   box.setAttribute("data-i", i);
   box.classList.add("box");
-
+  console.log(level);
+  if (level == 10) {
+    box.classList.add("box-10");
+  } else if (level == 9) {
+    box.classList.add("box-9");
+  } else if (level == 7) {
+    box.classList.add("box-7");
+  }
   box.addEventListener("click", function () {
     this.classList.toggle("clicked");
     console.log(this.getAttribute("data-i"));
--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-const difficultLvl = document.getElementById("difficult");
+const difficultLvlInput = document.getElementById("difficult");
 const startBtn = document.getElementById("start");
 const gameboard = document.getElementById("gameboard");
 
 startBtn.addEventListener("click", function () {
-  generateGrid(gameboard);
+  difficultLvl = parseInt(difficultLvlInput.value);
+  generateGrid(gameboard, difficultLvl);
 });
