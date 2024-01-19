8d5649c626dfeb800a0a4a5e535c988e10a85beb
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-19 15:29:42 +0100

    feat: inserimento bombArray con numeri unici

--- js/main.js
+++ js/main.js
@@ -4,5 +4,18 @@ const gameboard = document.getElementById("gameboard");
 
 startBtn.addEventListener("click", function () {
   difficultLvl = parseInt(difficultLvlInput.value);
+  const bombArray = [];
+  let min = 1;
+  let max = difficultLvl * difficultLvl;
+  let offset = max - min;
+  if (offset >= 16) {
+    while (bombArray.length < 16) {
+      let bomb = Math.floor(Math.random() * (max - min + 1) + min);
+      if (!bombArray.includes(bomb)) {
+        bombArray.push(bomb);
+      }
+    }
+  }
+  console.table(bombArray);
   generateGrid(gameboard, difficultLvl);
 });
